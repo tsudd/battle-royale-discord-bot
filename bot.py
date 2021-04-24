69e7fd8cab0fc85826d83870a3a92ea44ebf8ca3
Alexander Khristsenka <alex.kr1stehko@gmail.com>
2021-04-24 18:52:55 +0300

    game launch

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import logging
 import discord
+import json
+import time as tm
 from discord import utils
 from discord.ext import commands
 from config import *
+from entities.quiz import Quiz
 
 from info import TOKEN
 
@@ -10,10 +13,16 @@ from info import TOKEN
 class EqualizerBot(commands.Bot):
     def __init__(self, command_prefix=COMMAND_PREFIX, self_bot=False):
         commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=self_bot)
+        self.battles = []
+        self.broadcast_channel = None
+        self.admin_channel = None
+        self.question_base = EqualizerBot.load_questions()
         self.link_commands()
 
     async def on_ready(self):
         logging.info(f"Bot creation - succeed. Logged as {self.user}")
+        self.broadcast_channel = await self.fetch_channel(BROADCAST_CHANNEL)
+        self.admin_channel = await self.fetch_channel(ADMIN_CHANNEL)
 
     def link_commands(self):
 
@@ -24,6 +33,87 @@ class EqualizerBot(commands.Bot):
             for st in COMMANDS:
                 ans += st + '\n'
             await ctx.channel.send(ans)
+
+        @self.command(name="startbattle", pass_context=True)
+        async def start_battle(ctx, *args):
+            if ctx.channel.id == ADMIN_CHANNEL:
+                try:
+                    topics = self.parse_topics([*args])
+                    if len(topics) == 0:
+                        await self.admin_channel.send(NO_ARGUMENTS)
+                        return
+                except ValueError:
+                    await self.admin_channel.send(WRONG_ARGUMENTS_START)
+                    return
+                logging.info(f"Starting battle with topics {topics}")
+                role = await self.crate_arena_role(ctx.guild)
+                text_channel = await self.create_battle_channel(ctx.guild, role)
+                players = await self.get_players(text_channel, topics, SECONDS_TO_JOIN)
+                new_battle = Quiz(text_channel.id, ctx.me, players, topics, self.question_base)
+                self.battles.append(new_battle)
+                await self.launch_game(new_battle)
+
+    def parse_topics(self, args):
+        topics = []
+        for arg in args:
+            if arg in QUESTION_FLAGS:
+                topics.append(QUESTION_FLAGS[arg])
+            else:
+                raise ValueError
+        return topics
+
+    async def get_players(self, channel, topics=[], time=15):
+        players = []
+        message = await self.broadcast_invite(channel, time)
+        logging.info(f"Sent invite for {channel.name}.")
+        await message.add_reaction(JOIN_EMOJI)
+        tm.sleep(time)
+        message = await self.broadcast_channel.fetch_message(message.id)
+        if join_react := message.reactions[0]:
+            async for user in join_react.users():
+                players.append(user)
+        logging.info(f"List of players to join - {players}")
+        return players
+
+    async def broadcast_invite(self, channel, time, salt=""):
+        return await self.broadcast_channel.send(ARENA_INVITE % (channel.name, time))
+
+    async def launch_game(self, quiz: Quiz):
+        channel = await self.fetch_channel(quiz.cid)
+
+
+
+    def send_message_admin(self):
+        pass
+
+    async def create_battle_channel(self, guild, role):
+        name = BATTLE_CHANNEL_TEMPLATE % (len(self.battles) + 1)
+
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=True),
+            role: discord.PermissionOverwrite(read_messages=True)
+        }
+
+        channel = await guild.create_text_channel(name, overwrites=overwrites)
+        logging.info(f"Created channel {name} with {channel.id}.")
+        return channel
+
+    async def crate_arena_role(self, guild):
+        role_name = BATTLE_ROLE_TEMPLATE % (len(self.battles) + 1)
+        # add random color generation
+        role = await guild.create_role(name=role_name)
+        logging.info(f"{role_name} role was created. {role}")
+        return role
+
+    @staticmethod
+    def load_questions():
+        fp = open(PATH_TO_QUESTIONS_FILE, "r")
+        ans = json.load(fp)
+        fp.close()
+        return ans
+
+
 
         # self.add_command(info)
 
--- config.py
+++ config.py
@@ -1,4 +1,13 @@
-JOIN_EMOTE = '👍'
+JOIN_EMOJI = '👍'
+ONE_EMOJI = '1️⃣'
+TWO_EMOJI = '2️⃣'
+THREE_EMOJI = '3️⃣'
+FOUR_EMOJI = '4️⃣'
+FIVE_EMOJI = '5️⃣'
+SIX_EMOJI = '6️⃣'
+SEVEN_EMOJI = '7️⃣'
+EIGHT_EMOJI = '8️⃣'
+NINE_EMOJI = "9️⃣"
 
 COMMAND_PREFIX = "!"
 
@@ -7,20 +16,38 @@ COMMANDS = [
     '/startbattle - Starts new battle',
     '/questions - Shows info about existing questions in lib',
     '/cancelbattle - Cancels an existing battle',
-    '/join - Joins an existing game'
+    '/clean - Cleans channel after ended games'
 ]
 
 QUESTION_TYPES = [
     "docker",
     "python",
     "git",
-    "django/flusk"
+    "django",
+    "flusk"
 ]
+
+QUESTION_FLAGS = {
+    "d": "docker",
+    "p": "python",
+    "g": "git",
+    "dj": "django",
+    "fl": "flusk"
+}
+
+QUESTION_EMOJI = {
+    "docker": "docker🐋",
+    "python": "python🐍",
+    "git": "git💾",
+    "flusk": "flusk🤯",
+    "django": "django🐘"
+}
 
 QUESTION_STRING_FIELD = 'question'
 QUESTION_ANSWERS_FIELD = 'answers'
 
 QUESTION_DESCRIPTION_FIELD = "description"
+PATH_TO_QUESTIONS_FILE = "questions.json"
 
 PRINT_HL = "-" * 50 + '\n'
 WINNERS_AMOUNT = 1
@@ -32,3 +59,17 @@ RULES_MESSAGE = f"Hello everyone! If you read this, it means you are on the batt
               f"attention, because if you are wrong, you will be kicked out of the game. Try to become the king of " \
               f"Arena or answer all questions. Good luck!!!\n\n"
 
+ARENA_INVITE = "Arena in %s will start in %d seconds. React below to join!"
+WRONG_ARGUMENTS_START = "Unable to create new battle: wrong arguments"
+NO_ARGUMENTS = "Couldn't execute this command: no arguments. Watch !info."
+
+
+ADMIN_CHANNEL = 833201507888267265
+BROADCAST_CHANNEL = "835537204188545073"
+
+SECONDS_TO_JOIN = 5
+
+BATTLE_CHANNEL_TEMPLATE = "Arena #%d"
+BATTLE_ROLE_TEMPLATE = "Arena %d warrior"
+
+GUILD_TOKEN = '833201505346650132'
